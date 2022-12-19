79c7c6c0c2056677a8575c37d78f85f804b49c0d
FIzepe <felipe.izepe@faire.com>
2022-12-19 14:28:49 -0300

    Initial Commit

---
+++ MemoryManagmentDemo/MemoryManagmentDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryManagmentDemoApp.swift
+//  MemoryManagmentDemo
+//
+//  Created by Felipe Izepe on 19/12/22.
+//
+
+import SwiftUI
+
+@main
+struct MemoryManagmentDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
